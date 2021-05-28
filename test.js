aa30ab1e6dcc9756b6ffb43c938d76697a8c5830
Jerry M <developer.jerrymannel@gmail.com>
2021-05-28 19:58:17 +0530

    brining in smart substituition

--- test.js
+++ test.js
@@ -1,20 +1,44 @@
-let a = {
-	a: "apple",
-	value: "apple"
+const { json } = require("express");
+const got = require("got");
+const e = require("./lib/data.validators");
+const validator = require("./lib/data.validators")
+
+let data = {
+	"_id": "60a896e2ad16052b2d774054",
+	"name": "Login",
+	"request": {
+		"body": {
+			"password": "thisismysecret",
+			"username": "jerry@appveen.com"
+		},
+		"headers": {
+			"content-type": "application/json; charset=utf-8"
+		},
+		"method": "POST",
+		"responseCode": 200,
+		"uri": "/api/a/rbac/login"
+	},
+	"url": "https://staging.appveen.com"
 }
 
-b = { ...a }
+async function init() {
+	try {
+		const options = {
+			url: `${data.url}${data.request.uri}`,
+			method: data.request.method,
+			searchParams: data.request.params,
+			headers: data.request.headers,
+		};
+		if (validator.whatIsThis(data.request.body) == 1 || validator.whatIsThis(data.request.body) == 2) options["json"] = data.request.body
+		else options["body"] = data.request.body
+		console.log(options)
+		// if (_payload.request.body) options['body'] = _payload.request.body;
+		response = await got(options);
+		console.log(response.statusCode)
+		console.log(response.body)
+	} catch (error) {
+		console.log(error)
+	}
+}
 
-b.a = "ball"
-
-console.log(a)
-console.log(b)
-let { value } = a
-console.log({ value })
-
-
-// return {
-// 	req.operationDoc.parameters.body,
-// 	req.operationDoc.parameters.query,
-// 	req.operationDoc.parameters.params,
-// }
+init()
